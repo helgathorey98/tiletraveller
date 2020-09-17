a9cdc27eca5485a7f683cebf65f4f0eaa383dc4f
Helga Þórey <helgathorey@Helgas-MacBook.local>
2020-09-17 16:03:53 +0000

    nytt

---
+++ tiletraveller.py
@@ -0,0 +1,9 @@
+#Algoritm:
+# Player starts in tile [1,1] from this tile the user can only go north. From [1,2] he can go all directions except 
+# west. [1,1], [2,1] and [3,1] are the same because they can only go one north. [2,2] and [3,3] are the same because 
+# they can go south and west.[1,3] can go south and east. [2,3] can go west and east. 
+# We need to take in each tile starting from [1,1] and we inform the user which directions are possible and then he 
+# can choose which direction he will go to. If he chooses an invalid direction we will inform him about that.
+
+
+
